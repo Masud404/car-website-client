ca17b96e2786a8a7107aa201d0c0588cc5a2eb4b
Masud404 <masud215502@gmail.com>
2021-11-11 00:44:51 +0600

    connect to git

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
       </header>
     </div>
---
+++ src/Firebase/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDNmgidOdwk6wILDVlrlqbhCqiETMuxlxQ",
+
+    authDomain: "car-website-e4ba6.firebaseapp.com",
+
+    projectId: "car-website-e4ba6",
+
+    storageBucket: "car-website-e4ba6.appspot.com",
+
+    messagingSenderId: "714472923021",
+
+    appId: "1:714472923021:web:684fe858fa5377ce5227c4"
+
+};
+export default firebaseConfig;
